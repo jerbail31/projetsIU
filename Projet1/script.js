90bd7d7a407a1580495c66d65051e269c11d5bac
[Jeremy Baillargeon] <jerbail31@gmail.com>
2023-05-29 00:18:14 -0400

    update

--- Projet1/script.js
+++ Projet1/script.js
@@ -2,3 +2,10 @@ function scrollRendezVous(){
     var elmntToView = document.getElementById("rendezVous");
     elmntToView.scrollIntoView(); 
 }
+function addBorder(){
+    var div = document.getElementById("divLocationHours");
+    div.style.border = "solid 3px red";
+    div.style.borderRadius = "25px";
+    
+    return false;
+}
