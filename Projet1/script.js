a435fdec99f1a4d236ebabdf69a9e561a3c0bf35
Jérémy Baillargeon <jerbail31@gmail.com>
2023-06-03 11:01:31 -0400

    projet1 update

--- Projet1/script.js
+++ Projet1/script.js
@@ -31,6 +31,11 @@ $(document).ready(function(){
     //tooltip init
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+    //popover
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
 });
 
 //Fausse validation
